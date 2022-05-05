e47f5386b400c0dc6f2f0e8d3906d58ef806d892
songmin <tft0720@gmail.com>
2022-05-05 17:30:34 +0900

    drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 
 /**
  * initial 원하는 Element의 초기 상태 입력
@@ -8,18 +9,27 @@ import { motion } from 'framer-motion';
 
 // property 이름은 상관없음
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: '100px' },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: '100px' },
+  dragging: { backgroundColor: 'rgb(46, 204, 113)' },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        variants={boxVariants}
-        whileHover='hover' //
-        whileTap='click'
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragConstraints={biggerBoxRef}
+          dragSnapToOrigin
+          dragElastic={0.5}
+          variants={boxVariants}
+          whileHover='hover' //
+          whileTap='click'
+          whileDrag='dragging'
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
@@ -33,12 +43,23 @@ const Wrapper = styled.div`
   background-color: #2e2e2e;
 `;
 
+const BiggerBox = styled.div`
+  width: 500px;
+  height: 500px;
+  border-radius: 40px;
+  background-color: rgba(255, 255, 255, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   display: grid;
-  width: 200px;
-  height: 200px;
+  width: 150px;
+  height: 150px;
   background-color: tomato;
-  border-radius: 40px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
