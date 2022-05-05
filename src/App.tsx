a98e50097041b5a09cacc5588a86913bd6863788
songmin <tft0720@gmail.com>
2022-05-05 18:51:53 +0900

    useMotionValue, useTransform

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import { useRef } from 'react';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 
 /**
  * initial 원하는 Element의 초기 상태 입력
@@ -9,56 +8,47 @@ import { useRef } from 'react';
 
 // property 이름은 상관없음
 const boxVariants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: '100px' },
   dragging: { backgroundColor: 'rgb(46, 204, 113)' },
 };
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-400, 400], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-400, 0, 400],
+    [
+      'linear-gradient(135deg, rgba(108, 0, 240, 0.9), rgb(240, 0, 164))',
+      'linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))',
+      'linear-gradient(135deg, rgb(0, 84, 240), rgb(0, 164, 240))',
+    ]
+  );
   return (
-    <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragConstraints={biggerBoxRef}
-          dragSnapToOrigin
-          dragElastic={0.5}
-          variants={boxVariants}
-          whileHover='hover' //
-          whileTap='click'
-          whileDrag='dragging'
-        />
-      </BiggerBox>
+    <Wrapper style={{ background: gradient }}>
+      <Box
+        style={{ x, rotateZ }} //
+        drag='x'
+        dragSnapToOrigin
+        variants={boxVariants}
+        whileDrag='dragging'
+      />
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #2e2e2e;
-`;
-
-const BiggerBox = styled.div`
-  width: 500px;
-  height: 500px;
-  border-radius: 40px;
-  background-color: rgba(255, 255, 255, 0.7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
   display: grid;
   width: 150px;
   height: 150px;
-  background-color: tomato;
+  background-color: #fff;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
