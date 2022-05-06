f5955751c29edf823dae9d5305404707a8c9a45d
songmin <tft0720@gmail.com>
2022-05-06 09:27:46 +0900

    layoutId modal

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { useState } from 'react';
 // property 이름은 상관없음
 
 function App() {
-  const x = useMotionValue(0);
+  const x = useMotionValue(-400);
   const gradient = useTransform(
     x,
     [-400, 0, 400],
@@ -20,16 +20,42 @@ function App() {
       'linear-gradient(135deg, rgb(0, 84, 240), rgb(0, 164, 240))',
     ]
   );
-  const [isClick, setIsClick] = useState(false);
-  const toggleClicked = () => setIsClick((prev) => !prev);
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+  const clickHandler = (id: string) => {
+    setSelectedId(id);
+  };
 
   return (
-    <Wrapper onClick={toggleClicked} style={{ background: gradient }}>
-      <Box>{!isClick ? <Circle layoutId='circle' style={{ borderRadius: '50px' }} /> : null}</Box>
-      <Box>{isClick ? <Circle layoutId='circle' style={{ borderRadius: '15px' }} /> : null}</Box>
+    <Wrapper style={{ background: gradient }}>
+      <Grid>
+        {[1, 2, 3, 4].map((num) => (
+          <Box onClick={() => clickHandler(String(num))} key={num} layoutId={String(num)} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {selectedId ? (
+          <Overlay
+            onClick={() => setSelectedId(null)}
+            initial={{ backgroundColor: 'rgba(0,0,0,0)' }} //
+            animate={{ backgroundColor: 'rgba(0,0,0,0.5)' }}
+            exit={{ backgroundColor: 'rgba(0,0,0,0)' }}
+          >
+            <Box layoutId={selectedId} style={{ width: 300, height: 200 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -39,23 +65,25 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
   display: flex;
-  width: 400px;
-  height: 400px;
-  background-color: rgba(255, 255, 255, 0.8);
+  height: 150px;
+  background-color: rgba(255, 255, 255, 1);
   justify-content: center;
   align-items: center;
   border-radius: 15px;
-  margin-left: 10px;
   font-size: 30px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.5), 0 10px 20px rgba(0, 0, 0, 0.02);
-`;
-
-const Circle = styled(motion.div)`
-  width: 100px;
-  height: 100px;
-  background-color: #00a5ff;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.5), 0 10px 20px rgba(0, 0, 0, 0.02);
 `;
 
